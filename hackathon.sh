f32e541884aa1cd44c7841fe7a0b68542b60487c
William Gu <williamgusien@gmail.com>
2016-05-06 15:25:24 -0700

    add more bosh commands

--- hackathon.sh
+++ hackathon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT_DIR="bosh_logs_`date +%Y%m%d%H%M%S.%N`"
+OUTPUT_DIR="bosh_logs_`date +%Y%m%d%H%M%S`"
 
 if !command -v bosh > /dev/null 2>&1; then
   echo "Error: bosh command not found"
@@ -22,9 +22,9 @@ while [[ $# > 1 ]]; do
       PASSWORD="$2"
       shift
       ;;
-    # -j|--job)
-      #   JOB = "$2"
-      #   shift;;
+    -j|--job)
+        JOB = "$2"
+        shift;;
     -l|--upload)
       UPLOAD="$2"
       shift
@@ -54,17 +54,49 @@ bosh releases > $OUTPUT_DIR/releases.log
 bosh stemcells > $OUTPUT_DIR/stemcells.log
 echo "Available deployments:"
 bosh deployments | tee $OUTPUT_DIR/deployments.log
+
 read -p "Select deployment: " DEPLOYMENT
-bosh download manifest $DEPLOYMENT $OUTPUT_DIR/$DEPLOYMENT.yml
-bosh deployment $OUTPUT_DIR/$DEPLOYMENT.yml
+DEPLOYMENT_YAML=$OUTPUT_DIR/$DEPLOYMENT.yml
+if [[ -n $DEPLOYMENT ]]; then
+  bosh download manifest $DEPLOYMENT $DEPLOYMENT_YAML
+  while [[ $? != 0 ]]; do
+    echo "Wrong deployment: $DEPLOYMENT"
+    read -p "Select deployment: " DEPLOYMENT
+    if [[ -n $DEPLOYMENT ]]; then
+      bosh download manifest $DEPLOYMENT $DEPLOYMENT_YAML
+    fi
+  done
+else
+  echo "Skipped fetching deployment information."
+fi
 
-# bosh instances —-details > $OUTPUT_DIR/instances.details.log
-# bosh instances —ps > $OUTPUT_DIR/instances.ps.log
+if [[ -s $DEPLOYMENT_YAML ]]; then
+  bosh deployment $DEPLOYMENT_YAML
+  bosh instances --ps > $OUTPUT_DIR/instances.ps.log 2>&1
+  bosh instances --details | tee $OUTPUT_DIR/instances.details.log 2>&1
+  read -p "Select job: " JOB
+  if [[ -n $JOB ]]; then
+    read -p "Select index: " JOB_INDEX
+    while [[ -z $JOB_INDEX ]]; do
+      read -p "Select index: " JOB_INDEX
+    done
+    bosh logs $JOB $JOB_INDEX --dir $OUTPUT_DIR > $OUTPUT_DIR/$JOB.$JOB_INDEX.log
+  else
+    echo "Skipped fetching job logs."
+  fi
+fi
 
-# bosh logs <job> <index> [—only filters]
-# bosh tasks recent <50> —no-filter [—deployment deployment_name]
-# bosh task <num> —debug/cpi
+bosh tasks recent 100 --no-filter > $OUTPUT_DIR/tasks.log
+read -p "Task ID: " TASK_ID
+if [[ -n TASK_ID ]]; then
+  bosh task $TASK_ID --debug > $OUTPUT_DIR/task.$TASK_ID.debug.log
+  bosh task $TASK_ID --event > $OUTPUT_DIR/task.$TASK_ID.event.log 2>&1
+  bosh task $TASK_ID --cpi > $OUTPUT_DIR/task.$TASK_ID.cpi.log 2>&1
+else
+  echo "Skipped fetching task logs."
+fi
 
 # Package into a tarball
+tar czf $OUTPUT_DIR.tgz $OUTPUT_DIR
 
 # Upload
