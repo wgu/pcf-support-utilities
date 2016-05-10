bda4f16f7cc0287a0a5fd6833dfef2557e6605d6
William Gu <williamgusien@gmail.com>
2016-05-10 11:23:56 -0700

    enable  argument

--- hackathon.sh
+++ hackathon.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+set -e
+
 OUTPUT_DIR="pcf_support_`date +%Y%m%d%H%M%S`"
 
-if !command -v bosh > /dev/null 2>&1; then
-  echo "Error: bosh command not found"
+if [[ -z $BOSH_CMD ]]; then
+  BOSH_CMD=bosh
+else
+  BOSH_CMD="eval $BOSH_CMD"
+fi
+if ! command -v $BOSH_CMD > /dev/null 2>&1; then
+  echo "Error: $BOSH_CMD not found"
   exit 1
 fi
 
@@ -38,36 +46,36 @@ while [[ $# > 1 ]]; do
 done
 
 if [[ -z $TARGET ]]; then
-  # echo "Error: Missing bosh target"
+  # echo "Error: Missing $BOSH_CMD target"
   # exit 1
   read -p "Target: " TARGET
 fi
 
 # Login
-bosh target $TARGET
-bosh login $USERNAME $PASSWORD
+$BOSH_CMD target $TARGET
+$BOSH_CMD login $USERNAME $PASSWORD
 
 mkdir -p $OUTPUT_DIR
 
-# Run Bosh commands
-echo "Fetching Bosh status..."
-bosh status > $OUTPUT_DIR/status.log 2>&1
+# Run $BOSH_CMD commands
+echo "Fetching bosh status..."
+$BOSH_CMD status > $OUTPUT_DIR/status.log 2>&1
 echo "Fetching releases..."
-bosh releases > $OUTPUT_DIR/releases.log 2>&1
+$BOSH_CMD releases > $OUTPUT_DIR/releases.log 2>&1
 echo "Fetching stemcells..."
-bosh stemcells > $OUTPUT_DIR/stemcells.log 2>&1
+$BOSH_CMD stemcells > $OUTPUT_DIR/stemcells.log 2>&1
 echo "Fetching available deployments..."
-bosh deployments 2>$OUTPUT_DIR/deployments.log | tee $OUTPUT_DIR/deployments.log
+$BOSH_CMD deployments 2>$OUTPUT_DIR/deployments.log | tee $OUTPUT_DIR/deployments.log
 
 read -p "Select deployment: " DEPLOYMENT
 DEPLOYMENT_YAML=$OUTPUT_DIR/$DEPLOYMENT.yml
 if [[ -n $DEPLOYMENT ]]; then
-  bosh download manifest $DEPLOYMENT $DEPLOYMENT_YAML > $OUTPUT_DIR/download_manifest.log 2>&1
+  $BOSH_CMD download manifest $DEPLOYMENT $DEPLOYMENT_YAML > $OUTPUT_DIR/download_manifest.log 2>&1
   while [[ $? != 0 ]]; do
     echo "Wrong deployment: $DEPLOYMENT"
     read -p "Select deployment: " DEPLOYMENT
     if [[ -n $DEPLOYMENT ]]; then
-      bosh download manifest $DEPLOYMENT $DEPLOYMENT_YAML > $OUTPUT_DIR/download_manifest.log 2>&1
+      $BOSH_CMD download manifest $DEPLOYMENT $DEPLOYMENT_YAML > $OUTPUT_DIR/download_manifest.log 2>&1
     fi
   done
 else
@@ -76,11 +84,11 @@ fi
 
 if [[ -s $DEPLOYMENT_YAML ]]; then
   echo "Setting deployment..."
-  bosh deployment $DEPLOYMENT_YAML > $OUTPUT_DIR/deployment.log 2>&1
+  $BOSH_CMD deployment $DEPLOYMENT_YAML > $OUTPUT_DIR/deployment.log 2>&1
   echo "Fetching process information..."
-  bosh instances --ps > $OUTPUT_DIR/instances.ps.log 2>&1
+  $BOSH_CMD instances --ps > $OUTPUT_DIR/instances.ps.log 2>&1
   echo "Fetching instance details..."
-  bosh instances --details 2>$OUTPUT_DIR/instances.details.log | tee $OUTPUT_DIR/instances.details.log 2>&1
+  $BOSH_CMD instances --details 2>$OUTPUT_DIR/instances.details.log | tee $OUTPUT_DIR/instances.details.log 2>&1
   read -p "Select job: " JOB
   if [[ -n $JOB ]]; then
     read -p "Select index: " JOB_INDEX
@@ -88,22 +96,22 @@ if [[ -s $DEPLOYMENT_YAML ]]; then
       read -p "Select index: " JOB_INDEX
     done
     echo "Fetching job logs..."
-    bosh logs $JOB $JOB_INDEX --dir $OUTPUT_DIR > $OUTPUT_DIR/$JOB.$JOB_INDEX.log 2>&1
+    $BOSH_CMD logs $JOB $JOB_INDEX --dir $OUTPUT_DIR > $OUTPUT_DIR/$JOB.$JOB_INDEX.log 2>&1
   else
     echo "Skipped fetching job logs."
   fi
 fi
 
 echo "Fetching 100 most recent bosh task records..."
-bosh tasks recent 100 --no-filter > $OUTPUT_DIR/tasks.log 2>&1
+$BOSH_CMD tasks recent 100 --no-filter > $OUTPUT_DIR/tasks.log 2>&1
 read -p "Task ID: " TASK_ID
 if [[ -n $TASK_ID ]]; then
   echo "Fetching task debug log..."
-  bosh task $TASK_ID --debug > $OUTPUT_DIR/task.$TASK_ID.debug.log 2>&1
+  $BOSH_CMD task $TASK_ID --debug > $OUTPUT_DIR/task.$TASK_ID.debug.log 2>&1
   echo "Fetching task event log..."
-  bosh task $TASK_ID --event > $OUTPUT_DIR/task.$TASK_ID.event.log 2>&1
+  $BOSH_CMD task $TASK_ID --event > $OUTPUT_DIR/task.$TASK_ID.event.log 2>&1
   echo "Fetching task cpi log..."
-  bosh task $TASK_ID --cpi > $OUTPUT_DIR/task.$TASK_ID.cpi.log 2>&1
+  $BOSH_CMD task $TASK_ID --cpi > $OUTPUT_DIR/task.$TASK_ID.cpi.log 2>&1
 else
   echo "Skipped fetching task logs."
 fi
