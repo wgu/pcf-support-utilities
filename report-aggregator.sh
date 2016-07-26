e32aef2c961f11ea6e9a1ca842b3d7a64036a1af
William Gu <williamgusien@gmail.com>
2016-07-26 14:11:03 -0700

    Tweak the way to fetch opsman URL

--- report-aggregator.sh
+++ report-aggregator.sh
@@ -3,26 +3,25 @@
 set -e
 
 OUTPUT_DIR="pcf_support_`date +%Y%m%d%H%M%S`"
-UAAC_PATH="uaac"
+export BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/uaac/Gemfile
 
-if [[ -z $UAAC_PATH ]]; then
-  echo "This script requires uaac. Enter the path to it: "
-  read UAAC_PATH
-fi
+UAAC="bundle exec uaac --config support_uaac.yml"
+#alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
 
-UAAC="$UAAC_PATH --config support_uaac.yml"
+#echo "Please enter https://OPSMAN_ADDR"
+#read -p "OpsMan URL: " OPSMAN
+OPSMAN_HOSTNAME=$(sudo -u postgres psql -t -d tempest_production -c "SELECT hostname FROM uaa_configs WHERE id=1")
+OPSMAN_HOSTNAME=$(tr -d '[[:space:]]' <<< $OPSMAN_HOSTNAME)
 
-echo "Please enter https://OPSMAN_ADDR"
-read -p "OpsMan URL: " OPSMAN
+#$UAAC target https://om.stout.mini.pez.pivotal.io/uaa --skip-ssl-validation
+$UAAC target https://$OPSMAN_HOSTNAME/uaa --skip-ssl-validation
 
-$UAAC target $OPSMAN/uaa --skip-ssl-validation
-
-$UAAC token owner get opsman -s ""
+$UAAC token owner get opsman -s ''
 
 mkdir -p $OUTPUT_DIR
 echo "All information fetched will be put in $OUTPUT_DIR."
 
-OPAMAN_API=$OPSMAN/api/v0
+OPAMAN_API="https://$OPSMAN_HOSTNAME/api/v0"
 UAAC_CURL="$UAAC curl -k $OPAMAN_API"
 
 $UAAC_CURL/diagnostic_report > $OUTPUT_DIR/diagnostic_report.log 2>&1
@@ -45,4 +44,5 @@ do
     "x")
       exit 0
   esac
+  echo "Done."
 done
